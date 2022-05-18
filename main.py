870e7e9d3b525d07578fdb8d23fc93db099dad97
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-05-18 13:00:20 +0600

    Divide main script, add arguments check
    For good practice separate functional script.

--- main.py
+++ main.py
@@ -1,76 +1,22 @@
-import shutil
-import os
 import sys
+from commands import *
 
 # get command and arguments
 _, command, *args = sys.argv
 
 
-def init(_):
-    """
-    Initializes 'zeon_fs' directory
-    """
-    # get directory
-    cwd = os.getcwd()
-    # create 'zeon_fs' directory if it isn't exists
-    if not os.path.exists(cwd + '/zeon_fs'):
-        os.mkdir('zeon_fs')
+def check_args():
+    if len(args) > 0:
+        return True
+    else:
+        print('Here is no arguments!')
+        return None
 
 
-# try to go to the 'zeon_fs' directory
-try:
-    os.chdir('./zeon_fs')
-
-except FileNotFoundError:
-    pass
-
-
-def list_files(_):
-    """
-    Returns list of files included in 'zeon_fs' directory
-    """
-    os.system('ls')
-
-
-def add_file(path):
-    """
-    Copy file from given path to the 'zeon_fs' directory
-    """
-    source = f'{path}'
-    destination = '.'
-    shutil.copy(source, destination)
-
-
-def delete_file(file_name):
-    """
-    Delete file from 'zeon_fs' directory
-    """
-    os.remove(f'./{file_name}')
-
-
-def get_file_content(file_name, get_file_path):
-    """
-    Create new file in 'zeon_fs' directory.
-    The content of new file copied from file,
-    that given in the path as argument
-    """
-    content = open(get_file_path, 'r')
-    zeon_fs_file = open(f'./{file_name}', 'x')
-    zeon_fs_file.write(content.read())
-    zeon_fs_file.close()
-    content.close()
-
-
-# Commands Dictionary
-commands = {
-    'init': init,
-    'add': add_file,
-    'delete': delete_file,
-    'list': list_files,
-    'get_file': get_file_content
-}
+args = ''.join(args)
 
 if command in commands:
-    commands[command](''.join(args))
+    check_args()
+    commands[command](args)
 else:
-    commands['get_file'](command, ''.join(args))
+    commands['get_file'](args)

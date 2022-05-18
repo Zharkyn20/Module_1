7199c364754e5398a10a25e328e1e74c3fb632c1
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-05-18 19:02:22 +0600

    Update script

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from commands import *
 # get command and arguments
 _, command, *args = sys.argv
 
-args = ''.join(args)
+# args = ''.join(args)
 if command in commands_dict.keys():
     check_args(args)
-    commands_dict[command](args)
+    commands_dict[command](*args)
 else:
     help_me()
